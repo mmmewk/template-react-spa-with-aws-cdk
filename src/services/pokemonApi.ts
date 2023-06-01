ff949c108af477e072f9f5948d156ba7567b97a8
Matthew Koppe <mmmewk@gmail.com>
2023-06-01 11:46:59 -0700

    Update: pokemon api to use refactored base query

--- src/services/pokemonApi.ts
+++ src/services/pokemonApi.ts
@@ -2,7 +2,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { Pokemon } from "./pokemonTypes";
 import queryString from "query-string";
-import humps from "humps";
 import { withCamelizeKeys } from "../utils/api";
 
 // Define a service using a base URL and expected endpoints

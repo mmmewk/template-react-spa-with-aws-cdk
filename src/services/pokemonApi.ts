fb3387f3a86e5a2002121b17f2898ac2a2c0af53
Matthew Koppe <mmmewk@gmail.com>
2023-06-01 11:46:17 -0700

    Update: pokemon api to use refactored base query

--- src/services/pokemonApi.ts
+++ src/services/pokemonApi.ts
@@ -3,19 +3,14 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { Pokemon } from "./pokemonTypes";
 import queryString from "query-string";
 import humps from "humps";
+import { withCamelizeKeys } from "../utils/api";
 
 // Define a service using a base URL and expected endpoints
 export const pokemonApi = createApi({
   reducerPath: "pokemonApi",
-  baseQuery: async (...params) => {
-    const query = fetchBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" });
-    const response = await query(...params);
-
-    // By default camelize response keys
-    if (response.data) response.data = humps.camelizeKeys(response.data);
-
-    return response;
-  },
+  baseQuery: withCamelizeKeys(
+    fetchBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" })
+  ),
   endpoints: (builder) => ({
     indexPokemon: builder.query<
       {
